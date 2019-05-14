3ec29f95befe6548b94fdcc9911f5714b04a34e7
Ralph L <lettau.ralph@gmail.com>
2019-05-14 20:52:18 +0200

    JUnit Extension Mocks

---
+++ src/test/java/guru/springframework/JUnitExstensionsTest.java
@@ -0,0 +1,23 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Map;
+
+@ExtendWith(MockitoExtension.class)
+public class JUnitExstensionsTest {
+
+    @Mock
+    Map<String, Object> mapMock;
+
+
+    @Test
+    void testMock() {
+        mapMock.put("keyvalue", "foo");
+    }
+
+}
+
