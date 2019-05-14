27bd1dd597cd6db3f7efe0c70c9469af7d7a9b9f
Ralph L <lettau.ralph@gmail.com>
2019-05-14 21:16:27 +0200

    Vet Service Assignemt

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/VetSDJpaServiceTest.java
@@ -0,0 +1,30 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.repositories.VetRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class VetSDJpaServiceTest {
+
+    @Mock
+    VetRepository vetRepository;
+
+    @InjectMocks
+    VetSDJpaService vetSDJpaService;
+
+
+    @Test
+    void deleteById() {
+
+        vetSDJpaService.deleteById(10l);
+
+        verify(vetRepository, times(1)).deleteById(10l);
+    }
+}
